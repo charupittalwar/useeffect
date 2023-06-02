a858715886190e64185dcc5c105b1f9b911a1e94
charupittalwar <charu4pittalwar@gmail.com>
2023-06-02 17:58:31 +0530

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import User from './User/User';
+
+function App() {
+  return (
+    <div>
+   <User>
+
+    <p>new value</p>
+   </User>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/User/User.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+const User:React.FC<any> = (props) => {
+
+   const [name , setName] =useState<string> ('php ');
+
+   const [pass , setPass] = useState<number> (0)
+
+const [isError , setIserror] = useState <any> ({status :false , msg :""})
+
+const chkerror = ( msg = 'default msg') => {
+
+alert(isError.msg);
+
+}
+
+useEffect (() =>{
+
+if (!isError.status && name =="php"){
+
+    chkerror();
+}
+else if (isError.status){
+
+chkerror(isError.msg)
+
+}
+
+
+}, [isError]);
+
+const login = () =>{
+
+
+    if (name ==="php"){
+setIserror({status :true ,msg :"name can not be php"})
+
+return;
+    }
+
+    console.log (name , pass)
+
+}
+
+ return (
+<div>
+
+
+<h1>user component</h1>
+{props.children}
+
+<input type = "text" value ={name} onChange={e =>setName(e.target.value)} ></input> <br></br><br></br>
+
+<input type = "number" value={pass} onChange={e =>setPass (parseInt(e.target.value))} ></input><br></br><br></br>
+
+<button onClick={login}>Login</button>
+
+</div>
+
+
+
+ )
+
+
+}
+
+export default User;
